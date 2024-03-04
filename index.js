427d43e05bafbcab00f2109aaeed2c5532ea98ea
Deniz Lapsekili <deniz@169-231-62-215.wireless.ucsb.edu>
2024-03-04 14:23:58 -0800

    EJS to node connection

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 import express from "express";
 import axios from "axios";
-import "dotenv/config";
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 const X_Auth_Token = process.env.X_Auth_Token;
 
+const config = {
+  headers: {
+    "X-Auth-Token": `${X_Auth_Token}`,
+  },
+};
+
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+function extract_short_names(data) {
+  const teamShortNames = data.standings[0].table.map(
+    (team) => team.team.shortName
+  );
+  return teamShortNames;
+}
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/league-select", async (req, res) => {
+  const selected_league = req.body.league;
+  const URL = `http://api.football-data.org/v4/competitions/${selected_league}/standings`;
+
+  try {
+    const result = await axios.get(URL, config);
+    let debug = extract_short_names(result);
+    console.log(debug);
+  } catch (error) {
+    console.log(`Exited with Error Code ${error}`);
+  }
 });
 
 app.listen(PORT, () => {
