e845a1da1967b228634d03e0b551af216991ba56
Deniz Lapsekili <deniz@169-231-62-215.wireless.ucsb.edu>
2024-03-05 18:03:30 -0800

    Complete Node.js Backend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import axios from "axios";
-require("dotenv").config();
+import "dotenv/config";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const X_Auth_Token = process.env.X_Auth_Token;
+const X_Auth_Token = process.env.TOKEN;
+
+let short_names = [];
 
 const config = {
   headers: {
@@ -15,11 +17,14 @@ const config = {
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
-function extract_short_names(data) {
-  const teamShortNames = data.standings[0].table.map(
-    (team) => team.team.shortName
-  );
-  return teamShortNames;
+function short_name_array(result) {
+  short_names = [];
+  for (let i = 0; i < result.data.standings[0].table.length; i++) {
+    short_names.push(result.data.standings[0].table[i].team.shortName);
+  }
+  return short_names;
+
+  // console.log(result.data.standings[0].table[0].team.shortName);
 }
 
 app.get("/", (req, res) => {
@@ -28,12 +33,13 @@ app.get("/", (req, res) => {
 
 app.post("/league-select", async (req, res) => {
   const selected_league = req.body.league;
-  const URL = `http://api.football-data.org/v4/competitions/${selected_league}/standings`;
-
   try {
-    const result = await axios.get(URL, config);
-    let debug = extract_short_names(result);
-    console.log(debug);
+    const result = await axios.get(
+      `http://api.football-data.org/v4/competitions/${selected_league}/standings`,
+      config
+    );
+    short_name_array(result);
+    console.log(short_names[1]);
   } catch (error) {
     console.log(`Exited with Error Code ${error}`);
   }
